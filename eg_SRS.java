8251498b7aa834973f493110586c4aefe0863e02
Apurbo04 <154694784+apurbo04@users.noreply.github.com>
2026-01-20 10:55:33 +0600

    Update eg_SRS.java

--- eg_SRS.java
+++ eg_SRS.java
@@ -1,64 +1,64 @@
-class sus_res_stop implements Runnable
-{
+class sus_res_stop implements Runnable {
     Thread th;
     boolean suspend_flag, stop_flag;
 
-    sus_res_stop(String name)
-    {
+    sus_res_stop(String name) {
         th = new Thread(this, name);
         suspend_flag = false;
         stop_flag = false;
         th.start();
     }
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             int j = 1;
-            while (j++ < 20)
-            {
-                synchronized (this)
-                {
-                    while (suspend_flag)
-                        wait();
 
-                    if (stop_flag)
+            while (j <= 20) {
+                synchronized (this) {
+                    while (suspend_flag) {
+                        System.out.println(th.getName() + " is suspended...waiting");
+                        wait();
+                    }
+
+                    if (stop_flag) {
+                        System.out.println(th.getName() + " detected stop_flag = true, stopping now...");
                         break;
+                    }
                 }
+
+                // Actual work + print
+                System.out.println("From Thread " + th.getName() + " : j = " + j);
+                j++;
+
+                // slow down to observe execution clearly
+                Thread.sleep(300);
             }
-        }
-        catch (InterruptedException e)
-        {
-            System.out.println("Thread interrupted");
+
+            System.out.println("Exit from Thread " + th.getName());
+        } catch (InterruptedException e) {
+            System.out.println(th.getName() + " interrupted");
         }
     }
 
-    synchronized void my_suspend()
-    {
+    synchronized void my_suspend() {
         suspend_flag = true;
     }
 
-    synchronized void my_resume()
-    {
+    synchronized void my_resume() {
         suspend_flag = false;
         notify();
     }
 
-    synchronized void my_stop()
-    {
+    synchronized void my_stop() {
         suspend_flag = false;
         stop_flag = true;
         notify();
     }
 }
 
-public class eg_SRS
-{
-    public static void main(String args[])
-    {
-        try
-        {
+public class eg_SRS {
+    public static void main(String args[]) {
+        try {
             sus_res_stop S_R_S_T = new sus_res_stop("SRS");
 
             System.out.println("Thread S_R_S_T is created and started");
@@ -74,9 +74,7 @@ public class eg_SRS
 
             S_R_S_T.my_stop();
             System.out.println("Thread S_R_S_T is stopped");
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             System.out.println("Generated interrupted exception");
         }
     }
